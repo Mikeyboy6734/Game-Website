820abab677acc3236a9a36404f2596876c46e95a
Mikeyboy6734 <Mikeygett@outlook.com>
2025-10-24 20:20:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const games = [
   { name: "Flappy Bird", thumbnail: "assets/images/flappy.png", path: "/games/flappy/index.html" },
-  { name: "Super Mario 64", thumbnail: "assets/images/sm64.jpeg", path: "games/Super Mario 64/index.html" }
+  { name: "Super Mario 64", thumbnail: "assets/images/sm64.jpeg", path: "/games/Super Mario 64/index.html" }
 ];
 
 const grid = document.getElementById('game-grid');
 
 games.forEach(game => {
   const card = document.createElement('a');
-  card.href = `${game.path}.html`;
+  card.href = game.path; // Removed extra .html
   card.className = 'game-card';
   card.innerHTML = `
     <img src="${game.thumbnail}" alt="${game.name}">
